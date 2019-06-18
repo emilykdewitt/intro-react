a0ace331699ae24e44ddaf4d60b4b210f964659d
Emily DeWitt <emilykdewitt@gmail.com>
2019-06-17 19:22:56 -0500

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,25 @@
+/* eslint-disable lines-between-class-members */
+import React from 'react';
+
+import goats from './goats';
+
+import './App.scss';
+
+class App extends React.Component {
+  state = {
+    goats: [],
+  }
+  componentDidMount() {
+    this.setState({ goats });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>Goat Yoga</div>
+        {/* <GoatCorral goats={goats}/> */}
+      </div>
+    );
+  }
+}
+
+export default App;
